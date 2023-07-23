a53544f081fe5431fbde33a1f5c6a8aae0c55098
kotomi1338 <kotomin1216@gmail.com>
2023-07-23 15:47:30 +0900

    ['ISUTAR_DSN']を追加

--- webapp/php/isuda.php
+++ webapp/php/isuda.php
@@ -3,6 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/lib/glue.php';
 
 $_ENV['ISUDA_DSN'] = 'mysql:host=127.0.0.1;dbname=isuda';
+$_ENV['ISUTAR_DSN'] = 'mysql:host=127.0.0.1;dbname=isutar';
 $_ENV['PHP_TEMPLATE_PATH'] = __DIR__ . '/views';
 
 $_SERVER += ['PATH_INFO' => $_SERVER['REQUEST_URI']];
@@ -17,7 +18,8 @@ if (is_file($file)) {
     ][pathinfo($file, PATHINFO_EXTENSION)] ?? false;
     if ($mimetype) {
         header("Content-Type: {$mimetype}");
-        echo file_get_contents($file); exit;
+        echo file_get_contents($file);
+        exit;
     }
 }
 
