46faef0e0a05860fc412c767714fdeea96b01c3e
IkezoeMakoto <150283t@gmail.com>
2023-07-23 12:32:15 +0900

    ベンチ実行時にslow queryもリセット

--- bench.sh
+++ bench.sh
@@ -3,7 +3,9 @@
 # root権限で実行してください
 
 systemctl restart isuda.php.service isutar.php.service nginx.service mysql.service
-echo '' > /var/log/nginx/error.log && echo '' > /var/log/nginx/access.log && echo '' > /home/isucon/.local/php/var/log/isuda.php-fpm.log && echo '' > /home/isucon/.local/php/var/log/isutar.php-fpm.log
+echo '' > /var/log/mysql/mysql-slow.sql && echo '' > /var/log/mysql/error.log && \
+echo '' > /var/log/nginx/error.log && echo '' > /var/log/nginx/access.log && \
+echo '' > /home/isucon/.local/php/var/log/isuda.php-fpm.log && echo '' > /home/isucon/.local/php/var/log/isutar.php-fpm.log
 
 # ベンチ実行
 su isucon -c 'cd isucon6q/ && ./isucon6q-bench'
